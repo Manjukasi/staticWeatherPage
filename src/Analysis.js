59a1a51c045001a293220c8baeac238d257e7b46
Manjukasi <mk970160@gmail.com>
2023-06-30 16:33:49 +0530

    Code reviewed

--- src/Analysis.js
+++ src/Analysis.js
@@ -1,194 +1,194 @@
-import CardComponent from "./CardComponent";
-import DataComponent from "./DataComponent";
+import CardComponent from './CardComponent'
+import DataComponent from './DataComponent'
+import React from 'react'
 
- export const weatherReport = [
-    {
-        id: 1,
+export const weatherReport = [
+  {
+    id: 1,
     weather: 'Humidity',
     icon: 'water_drop',
     cardClass: 'humidity',
     value: '82% Bad',
-    width: "60",
-    count: "3",
-    weatherState:[
-    {
-    label:['good'],
-    values: 100
-    },
-    {
-    label:['normal'],
-    values: 100
-    },
-    {
-    label:['bad'],
-    values: 30
-    }]
-},
-{
-    id:2,
+    width: '60',
+    count: '3',
+    weatherState: [
+      {
+        label: ['good'],
+        values: 100
+      },
+      {
+        label: ['normal'],
+        values: 100
+      },
+      {
+        label: ['bad'],
+        values: 30
+      }]
+  },
+  {
+    id: 2,
     weather: 'Wind',
     icon: 'air',
     cardClass: 'wind',
     value: '8 km/h',
-    width: "40",
-    count: "5",
-    weatherState:[
-        {
-        label:['0'],
+    width: '40',
+    count: '5',
+    weatherState: [
+      {
+        label: ['0'],
         values: 100
-        },
-        {
-        label:['5'],
+      },
+      {
+        label: ['5'],
         values: 100
-        },
-        {
-        label:['10'],
+      },
+      {
+        label: ['10'],
         values: 0
-        },
-        {
-        label:['20'],
+      },
+      {
+        label: ['20'],
         values: 0
-        },
-        {
-        label:['30'],
+      },
+      {
+        label: ['30'],
         values: 0
-        },
-        {
-        label:['40'],
+      },
+      {
+        label: ['40'],
         values: 0
-        },  
-    ]    
-},
-{
+      }
+    ]
+  },
+  {
     id: 3,
     weather: 'Precipitation',
     icon: 'thunderstorm',
     cardClass: 'precipitation',
-    value: "1.4 cm",
-    width: "23",
-    count: "10",
-    weatherState:[
-        {
-        label:['0'],
+    value: '1.4 cm',
+    width: '23',
+    count: '10',
+    weatherState: [
+      {
+        label: ['0'],
         values: 100
-        },
-        {
-        label:['10'],
+      },
+      {
+        label: ['10'],
         values: 90
-        },
-        {
-        label:['20'],
+      },
+      {
+        label: ['20'],
         values: 0
-        },
-        {
-        label:['30'],
+      },
+      {
+        label: ['30'],
         values: 0
-        },
-        {
-        label:['40'],
+      },
+      {
+        label: ['40'],
         values: 0
-        },
-        {
-            label:['50'],
-            values: 0
-            },  
-            {
-                label:['60'],
-                values: 0
-                },  
-                {
-                    label:['70'],
-                    values: 0
-                    },  
-                    {
-                        label:['80'],
-                        values: 0
-                        }, 
-                        {
-                            label:['90'],
-                            values: 0
-                            },                                            
+      },
+      {
+        label: ['50'],
+        values: 0
+      },
+      {
+        label: ['60'],
+        values: 0
+      },
+      {
+        label: ['70'],
+        values: 0
+      },
+      {
+        label: ['80'],
+        values: 0
+      },
+      {
+        label: ['90'],
+        values: 0
+      }
     ]
 
-},
-{
+  },
+  {
     id: 4,
     weather: 'UV dialog',
     icon: 'light_mode',
     cardClass: 'uv',
-    value: "4 medium",
-    width: "50",
-    count: "5",
-    weatherState:[
-        {
-        label:['0-2'],
+    value: '4 medium',
+    width: '50',
+    count: '5',
+    weatherState: [
+      {
+        label: ['0-2'],
         values: 100
-        },
-        {
-        label:['3-5'],
+      },
+      {
+        label: ['3-5'],
         values: 50
-        },
-        {
-        label:['6-7'],
+      },
+      {
+        label: ['6-7'],
         values: 0
-        },
-        {
-        label:['8-10'],
+      },
+      {
+        label: ['8-10'],
         values: 0
-        },
-        {
-        label:['11+'],
+      },
+      {
+        label: ['11+'],
         values: 0
-        },            
-    ]    
-},
-{
+      }
+    ]
+  },
+  {
     id: 5,
     weather: 'Feels like',
     icon: 'device_thermostat',
     cardClass: 'feel',
     value: '30°',
     width: '250',
-    count: "1",
-    weatherState:[
-        {
-        label:['0°', '25°', '50°'],
+    count: '1',
+    weatherState: [
+      {
+        label: ['0°', '25°', '50°'],
         values: 75
-        },
-        ]    
-},
-{
+      }
+    ]
+  },
+  {
     id: 6,
     weather: 'Chance of rain',
     icon: 'thunderstorm',
     cardClass: 'rain',
-    value: "42%",
+    value: '42%',
     width: '250',
-    count: "1",
-    weatherState:[
-        {
-        label:['0%', '25%', '50%', '75%', '100%'],
+    count: '1',
+    weatherState: [
+      {
+        label: ['0%', '25%', '50%', '75%', '100%'],
         values: 75
-        },
-        ]
-},
+      }
+    ]
+  }
 ]
 
 const Analysis = () => {
-
-    return (
-        <> 
-          <h3 className="details">More details of today's weather</h3>
-          <div className="analysis outline">
-            {weatherReport.map((weatherObj ,index) => {
-                const {id,weather, icon, cardClass,value,width,count, weatherState} = weatherObj;
-                return(
-                 <CardComponent key={id} id={id}  data={<DataComponent width={width} count={count} weatherState={weatherState}/>} weatherState={weatherState} weather={weather} icon={icon} cardClass={cardClass}  value ={value}/>
-                )
+  return (
+        <div>
+          <h3 className='details'>More detail`s of todays weather</h3>
+          <div className='analysis outline'>
+            {weatherReport.map((weatherObj, index) => {
+              const { id, weather, icon, cardClass, value, width, count, weatherState } = weatherObj
+              return (
+                 <CardComponent key={id} id={id} data={<DataComponent width={width} count={count} weatherState={weatherState}/>} weatherState={weatherState} weather={weather} icon={icon} cardClass={cardClass} value ={value}/>
+              )
             })
         }
-          </div>  
-        </>
-    )
+          </div>
+        </div>
+  )
 }
-export default Analysis;
+export default Analysis
--- src/Header.js
+++ src/Header.js
@@ -1,20 +1,19 @@
-
-
+import React from 'react'
 const Header = () => {
-    return (        
+  return (
    <div>
       <div className="header">
         <div className="sub-header">
          <h3>Welcome back Isabella</h3>
-         <p>Check out today's weather information</p>
+         <p>Check out today`s weather information</p>
         </div>
         <div className="profile">
         <span className="material-icons-outlined profile-status">more_horiz</span>
-          <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"  alt="pic" />
-        </div>    
+          <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" alt="pic" />
+        </div>
       </div>
     </div>
-    )
+  )
 }
 
-export default Header;
+export default Header
